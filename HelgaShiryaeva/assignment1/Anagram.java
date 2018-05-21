fb99e67b34ecafe6ef547882fcb3a3eb6f3464cf
Olga Shiryaeva <algorithmhell@gmail.com>
2018-05-21 23:21:33 +0300

    Created Anagram.java
    Updated Anagram.java class after first review

---
+++ HelgaShiryaeva/assignment1/Anagram.java
@@ -0,0 +1,78 @@
+package assignment1;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Anagram {
+    private static final String PUNCTUATION_MARKS = " ,?!.:";
+    private Map<Character, Integer> characterCountMap;
+
+    public Anagram() {
+        characterCountMap = new HashMap<>();
+    }
+    /**
+     * Checks if each word from the first string is an anagram of one of the words
+     * from the second one. Strings are considered to be anagrams this way.
+     * Supports punctuation marks : ?!,.:
+     * @param first is the first string to be checked if it is an anagram of
+     * @param second string
+     * @return true if each word from the first string is an anagram of one
+     * of the words from the second string
+     */
+    public boolean areAnagramsByWords(String first, String second) {
+        if (first.length() != second.length()) {
+            return false;
+        }
+        StringTokenizer stringTokenizerFirst = new StringTokenizer(first, PUNCTUATION_MARKS );
+        StringTokenizer stringTokenizerSecond = new StringTokenizer(second, PUNCTUATION_MARKS);
+        if (stringTokenizerFirst.countTokens() != stringTokenizerSecond.countTokens()) {
+            return false;
+        }
+        List<String> firstStringWords = new ArrayList<>();
+        while (stringTokenizerFirst.hasMoreTokens()) {
+            firstStringWords.add(stringTokenizerFirst.nextToken());
+        }
+        while (stringTokenizerSecond.hasMoreTokens()) {
+            String wordFromSecondString = stringTokenizerSecond.nextToken();
+            boolean isAnagram = false;
+            for (String wordFromFirstString : firstStringWords) {
+                if (areWordsAnagrams(wordFromFirstString, wordFromSecondString)) {
+                    isAnagram = true;
+                    break;
+                }
+            }
+            if (!isAnagram) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean areWordsAnagrams(String first, String second) {
+        characterCountMap.clear();
+        for (char c : first.toCharArray()) {
+            Integer count = characterCountMap.getOrDefault(c, -1);
+            if (count != -1) {
+                characterCountMap.put(c, count + 1);
+            } else {
+                characterCountMap.put(c, 1);
+            }
+        }
+        for (char c : second.toCharArray()) {
+            Integer count = characterCountMap.get(c);
+            if (count == null || count == 0) {
+                return false;
+            }
+            characterCountMap.put(c, characterCountMap.get(c) - 1);
+        }
+        for (Integer value : characterCountMap.values()) {
+            if (value != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
