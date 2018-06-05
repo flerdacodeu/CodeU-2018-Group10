744b330795548b3231e8033b20f9a142348db708
Yu Jo <zoey5538@gmail.com>
2018-06-06 00:43:20 +0100

    Add parent field to Node

--- uro98/assignment2/Node.java
+++ uro98/assignment2/Node.java
@@ -6,12 +6,14 @@ public class Node<T> {
   T data;
   Node<T> left;
   Node<T> right;
+  Node<T> parent;
   int depth;
   
   public Node(T data, int depth) {
     this.data = data;
     left = null;
     right = null;
+    parent = null;
     this.depth = depth;
   }
 }
