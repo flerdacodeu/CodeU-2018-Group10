cabebd17001ed72dc7734bb7b32508d8969559fb
Olga Shiryaeva <algorithmhell@gmail.com>
2018-06-08 01:04:57 +0300

    Main.java

--- HelgaShiryaeva/assignment2/Main.java
+++ HelgaShiryaeva/assignment2/Main.java
@@ -11,52 +11,29 @@ package assignment2;
  */
 public class Main {
     public static void main(String... args) {
-        try {
-            Node<Integer> root = new Node<>(4);
-            Node<Integer> node1 = new Node<>(1);
-            Node<Integer> node2 = new Node<>(2);
-            Node<Integer> node3 = new Node<>(3);
-            Node<Integer> node5 = new Node<>(5);
-            Node<Integer> node6 = new Node<>(6);
-            Node<Integer> node7 = new Node<>(7);
-            Node<Integer> node9 = new Node<>(9);
-            Node<Integer> node10 = new Node<>(10);
-            BinaryTree<Integer> binaryTree = new BinaryTree<>(root);
-            root.setLeft(node2);
-            root.setRight(node6);
-            node2.setLeft(node1);
-            node2.setRight(node3);
-            node3.setLeft(node9);
-            node9.setRight(node10);
-            node6.setLeft(node5);
-            node6.setRight(node7);
-            binaryTree.addNode(node2);
-            binaryTree.addNode(node6);
-            binaryTree.addNode(node1);
-            binaryTree.addNode(node3);
-            binaryTree.addNode(node9);
-            binaryTree.addNode(node10);
-            binaryTree.addNode(node5);
-            binaryTree.addNode(node7);
-            binaryTree.printAncestors(node10);
-            binaryTree.printAncestors(root);
-            binaryTree.printAncestors(node7);
-            System.out.println(binaryTree.getLca(root, node10).getValue());
-            System.out.println(binaryTree.getLca(root, root).getValue());
-            System.out.println(binaryTree.getLca(node10, node7).getValue());
-            System.out.println(binaryTree.getLca(node7, node10).getValue());
-            System.out.println(binaryTree.getLca(node7, node7).getValue());
-            System.out.println(binaryTree.getLca(node10, node2).getValue());
-            System.out.println(binaryTree.getLca(node7, node5).getValue());
+            BinaryTree<Integer> binaryTree = new BinaryTree<>(4);
+            binaryTree.add(2, 4, Side.LEFT);
+            binaryTree.add(6, 4, Side.RIGHT);
+            binaryTree.add(1, 2, Side.LEFT);
+            binaryTree.add(3, 2, Side.RIGHT);
+            binaryTree.add(9, 3, Side.LEFT);
+            binaryTree.add(10, 9, Side.RIGHT);
+            binaryTree.add(5,6, Side.LEFT);
+            binaryTree.add(7, 6, Side.RIGHT);
 
-            Node<Integer> integerNode1 = new Node<>(1);
-            Node<Integer> integerNode2 = new Node<>(2);
-            integerNode1.setLeft(integerNode2);
-            BinaryTree<Integer> binaryTree1 = new BinaryTree<>(integerNode1);
-            binaryTree1.addNode(integerNode2);
-            binaryTree1.printAncestors(integerNode2);
-        } catch (OutOfTreeException e) {
-            System.out.println(e.getMessage());
-        }
+            binaryTree.printAncestors(10);
+            binaryTree.printAncestors(4);
+            binaryTree.printAncestors(7);
+            System.out.println(binaryTree.getLca(4, 10));
+            System.out.println(binaryTree.getLca(4, 4));
+            System.out.println(binaryTree.getLca(10, 7));
+            System.out.println(binaryTree.getLca(7, 10));
+            System.out.println(binaryTree.getLca(7, 7));
+            System.out.println(binaryTree.getLca(10, 2));
+            System.out.println(binaryTree.getLca(7, 5));
+
+            BinaryTree<Integer> binaryTree1 = new BinaryTree<>(1);
+            binaryTree1.add(2, 1, Side.LEFT);
+            binaryTree1.printAncestors(2);
     }
 }
