b99cb01976520f5acadd0ae11afed5788b2599b9
Olga Shiryaeva <algorithmhell@gmail.com>
2018-06-08 01:04:14 +0300

    Update BinaryTree.java

--- HelgaShiryaeva/assignment2/BinaryTree.java
+++ HelgaShiryaeva/assignment2/BinaryTree.java
@@ -1,104 +1,166 @@
 package assignment2;
 
 import java.util.HashSet;
-import java.util.Optional;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 /**
  * This class represents a Binary tree data structure
  * @param <T> is a type of values stored in nodes of a tree
+ * All values stored in a binary tree are unique
  */
-public class BinaryTree <T> {
-    private Set<Node<T>> nodes;
-    private Node<T> root;
+public class BinaryTree<T> {
+    private Node root;
+    private Set<T> values;
 
-    public BinaryTree(Node<T> root) {
-        this.root = root;
-        nodes = new HashSet<>();
-        nodes.add(root);
+    public BinaryTree(T rootValue) {
+        this.root = new Node(rootValue);
+        values = new HashSet<>();
+        values.add(rootValue);
     }
 
     /**
-     * All the nodes of a tree should be inserted into a nodes set
-     * in order to avoid duplicates. A node which is not in the
-     * set of nodes is not considered to be a part of a binary tree.
-     * @param newNode is a node to be inserted in the nodes set
-     * @throws IllegalArgumentException if node is already in a binary tree or if
-     * the node with such value is already exists in the tree
-     */
-    public void addNode(Node<T> newNode) {
-        Optional<Node<T>> nodeOptional = nodes.stream()
-                .filter(e -> e.getValue().equals(newNode.getValue()))
-                .findFirst();
-        if (nodes.contains(newNode) || nodeOptional.isPresent()) {
-            throw new IllegalArgumentException("Node with value " + newNode.getValue() + " already exists in the tree");
+     * This method adds a new node in a binary tree
+     * If the parent of a new node already has a child on
+     * the requested side an old child would be replaced by a new one
+     * @param value is a value of a new node
+     * @param parent is a parent of the node
+     * @param side on which side of parent a new node should be added
+     * */
+    public void add(T value, T parent, Side side) {
+        if (value == null) {
+            throw new NullPointerException("'null' value can't be inserted in a tree");
         }
-        nodes.add(newNode);
+        Node newNode = new Node(value);
+        Node parentNode = findNodeByValue(parent);
+        if (side.equals(Side.LEFT)) {
+            if (parentNode.getLeft() != null) {
+                values.remove(parentNode.getLeft().getValue());
+            }
+            parentNode.setLeft(newNode);
+        } else {
+            if (parentNode.getRight() != null) {
+                values.remove(parentNode.getRight().getValue());
+            }
+            parentNode.setRight(newNode);
+        }
+        values.add(value);
+    }
+
+    private Node findNodeByValue(T value) {
+        if (!values.contains(value)) {
+            throw new NoSuchElementException("Node with value " + value + " doesn't exist in a binary tree");
+        }
+        return findNodeByValueRecursive(root, value);
+    }
+
+    private Node findNodeByValueRecursive(Node current, T value) {
+        if (current == null) {
+            return null;
+        }
+        if (current.getValue().equals(value)) {
+            return current;
+        }
+        Node left = findNodeByValueRecursive(current.getLeft(), value);
+        if (left != null) {
+            return left;
+        } else {
+            return findNodeByValueRecursive(current.getRight(), value);
+        }
     }
 
     /**
      * Prints all the ancestors ordered from parent of the node to the root of the tree
-     * @param node is a node which ancestors will be printed
-     * @throws OutOfTreeException if the reference for the node which
-     * is not in the tree found during tree traversal
+     * @param value is a value of the tree node which ancestors will be printed
+     * @throws NullPointerException if the node with such value doesn't exist in a tree
      */
-    public void printAncestors(Node<T> node) throws OutOfTreeException {
-        if (nodes.contains(node)) {
-            printAncestorsRecursive(node, root);
-            System.out.println();
-        } else {
-            throw new OutOfTreeException(node);
+    public void printAncestors(T value)  {
+        if (!values.contains(value)) {
+            throw new NoSuchElementException("Node with value " + value + " doesn't exist in a binary tree");
         }
+            printAncestorsRecursive(root, value);
+            System.out.println();
     }
 
-    private Node<T> printAncestorsRecursive(Node<T> node, Node<T> currentNode) throws OutOfTreeException {
+    private Node printAncestorsRecursive(Node currentNode, T value) {
         if (currentNode != null) {
-            if (!nodes.contains(currentNode)) {
-                throw new OutOfTreeException(currentNode);
-            }
-            if (currentNode.getValue().equals(node.getValue())) {
+            if (currentNode.getValue().equals(value)) {
                 return currentNode;
             }
-            Node<T> left = printAncestorsRecursive(node, currentNode.getLeft());
-            Node<T> right = printAncestorsRecursive(node, currentNode.getRight());
-            if (left == null && right == null) {
+            Node left = printAncestorsRecursive(currentNode.getLeft(), value);
+            if (left != null) {
+                System.out.print(currentNode.getValue() + " ");
+                return currentNode;
+            }
+            Node right = printAncestorsRecursive(currentNode.getRight(), value);
+            if (right == null) {
                 return null;
             } else {
                 System.out.print(currentNode.getValue() + " ");
                 return currentNode;
             }
         }
-         return null;
+        return null;
     }
 
     /**
-     * Finds the lowest common ansestor of two nodes of a binary tree
-     * @return node which is the lowest common ancestor of two nodes
-     * @throws OutOfTreeException if either fisrt or second or both node(s) are not a part
+     * Finds the lowest common ancestor of two nodes of a binary tree
+     * @return value of the node which is the lowest common ancestor of two nodes
+     * @throws NoSuchElementException if either first or second or both node(s) are not a part
      * of a binary tree
      */
-    public Node<T> getLca(Node<T> first, Node<T> second) throws OutOfTreeException {
-        if (!nodes.contains(first) || !nodes.contains(second)) {
-            throw new OutOfTreeException("Node(s} is(are) not present in a binary tree");
+    public T getLca(T first, T second) {
+        if (!values.contains(first) || !values.contains(second)) {
+            throw new NoSuchElementException("Node(s} is(are) not present in a binary tree");
         }
-        return getLcaRecursive(root, first, second);
+        return getLcaRecursive(root, first, second).getValue();
     }
 
-    private Node<T> getLcaRecursive(Node<T> current, Node<T> first, Node<T> second) throws OutOfTreeException {
+    private Node getLcaRecursive(Node current, T first, T second) {
         if (current == null) {
             return null;
         }
-        if(!nodes.contains(current)) {
-            throw new OutOfTreeException(current);
-        }
-        if (current == first || current == second) {
+        if (current.getValue() == first || current.getValue() == second) {
             return current;
         }
-        Node<T> left = getLcaRecursive(current.getLeft(), first, second);
-        Node<T> right = getLcaRecursive(current.getRight(), first, second);
+        Node left = getLcaRecursive(current.getLeft(), first, second);
+        Node right = getLcaRecursive(current.getRight(), first, second);
         if (left != null && right != null) {
             return current;
         }
         return left != null ? left : right;
+    }
+
+    /**
+     * This class represents a node of a binary tree
+     */
+        class Node {
+        private final T value;
+        private Node left;
+        private Node right;
+
+        Node(T value) {
+            this.value = value;
+        }
+
+        T getValue() {
+            return value;
+        }
+
+        Node getLeft() {
+            return left;
+        }
+
+        void setLeft(Node left) {
+            this.left = left;
+        }
+
+        Node getRight() {
+            return right;
+        }
+
+        void setRight(Node right) {
+            this.right = right;
+        }
     }
 }
