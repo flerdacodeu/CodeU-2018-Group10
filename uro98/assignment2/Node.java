2a7abcb4d7465bce1806a5bb82c4e0b54439cea5
Yu Jo <zoey5538@gmail.com>
2018-06-11 01:06:47 +0100

    Update the Node class

--- uro98/assignment2/Node.java
+++ uro98/assignment2/Node.java
@@ -3,17 +3,53 @@
  */
 public class Node<T> {
   
-  T data;
-  Node<T> left;
-  Node<T> right;
-  Node<T> parent;
-  int depth;
+  enum Side {
+    LEFT, RIGHT
+  }
   
-  public Node(T data, int depth) {
+  private T data;
+  private Node<T> left;
+  private Node<T> right;
+  private Node<T> parent;
+  
+  public Node(T data) {
     this.data = data;
-    left = null;
-    right = null;
-    parent = null;
-    this.depth = depth;
+  }
+
+  public T getData() {
+    return data;
+  }
+  
+  public Node<T> getLeft() {
+    return left;
+  }
+  
+  public Node<T> getRight() {
+    return right;
+  }
+  
+  public Node<T> getParent() {
+    return parent;
+  }
+
+  /**
+   * Adds node with given value as child of the current node on the specified side,
+   * sets the current node as the parent and returns the newly created child.
+   * @param data generic value of the child
+   * @param side left or right side of the current node
+   * @return the child node
+   */
+  public Node<T> addChild(T data, Side side) {
+    Node<T> child = new Node<T>(data);
+    
+    if (side == Side.LEFT) {
+      this.left = child;
+    } else {
+      this.right = child;
+    }
+    
+    child.parent = this;
+    
+    return child;
   }
 }
