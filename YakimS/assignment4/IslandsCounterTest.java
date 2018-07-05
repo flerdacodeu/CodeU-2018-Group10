bdf9f037dcca12189a073ff905fbb89635166df3
S. Yakim <ykmsharon@gmail.com>
2018-07-05 22:19:37 +0300

    Update IslandsCounterTest.java

--- YakimS/assignment4/IslandsCounterTest.java
+++ YakimS/assignment4/IslandsCounterTest.java
@@ -1,44 +1,44 @@
 package com.shaya.CodeUAss4Package;
 
-import org.junit.Assert;
+import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class IslandsCounterTest {
 
-    @org.junit.Test
+    @Test
     public void countIslandInExampleGrid()
     {
         IslandsCounter islandsCounter = new IslandsCounter();
 
         boolean[][] boolGrid = new boolean[][]
                 {
-                        { false , true, false, true},
-                        { true , true, false, false},
+                        { false , true, false, true },
+                        { true , true, false, false },
                         { false , false, true, false},
                         { false , false, true, false},
                 };
 
-        Assert.assertEquals(islandsCounter.countIsland(boolGrid), 3);
+        assertEquals(3, islandsCounter.countIslands(boolGrid));
     }
 
-    @org.junit.Test
+    @Test
     public void countIslandInAllWaterGrid()
     {
         IslandsCounter islandsCounter = new IslandsCounter();
 
         boolean[][] boolGrid = new boolean[][]
                 {
-                        { false , false, false, false},
-                        { false , false, false, false},
-                        { false , false, false, false},
-                        { false , false, false, false},
+                        { false , false, false, false },
+                        { false , false, false, false },
+                        { false , false, false, false },
+                        { false , false, false, false },
                 };
 
-        Assert.assertEquals(islandsCounter.countIsland(boolGrid), 0);
+        assertEquals(0 , islandsCounter.countIslands(boolGrid));
     }
 
-    @org.junit.Test
+    @Test
     public void countIslandInEmptyGrid()
     {
         IslandsCounter islandsCounter = new IslandsCounter();
@@ -51,38 +51,50 @@ public class IslandsCounterTest {
                         { },
                 };
 
-        Assert.assertEquals(islandsCounter.countIsland(boolGrid), 0);
+        assertEquals( 0, islandsCounter.countIslands(boolGrid));
     }
 
-    @org.junit.Test
+    @Test
     public void countIslandInAllLandGrid()
     {
         IslandsCounter islandsCounter = new IslandsCounter();
 
         boolean[][] boolGrid = new boolean[][]
                 {
-                        { true , true, true, true},
-                        { true , true, true, true},
-                        { true , true, true, true},
-                        { true , true, true, true},
+                        { true , true, true, true },
+                        { true , true, true, true },
+                        { true , true, true, true },
+                        { true , true, true, true },
                 };
 
-        Assert.assertEquals(islandsCounter.countIsland(boolGrid), 1);
+        assertEquals( 1 , islandsCounter.countIslands(boolGrid));
     }
 
-    @org.junit.Test
+    @Test
     public void countDiagonalIslandsGrid()
     {
         IslandsCounter islandsCounter = new IslandsCounter();
 
         boolean[][] boolGrid = new boolean[][]
                 {
-                        { true , false, true, false},
-                        { false , true, false, true},
-                        { true , false, true, false},
-                        { false , true, false, true},
+                        { true , false, true, false },
+                        { false , true, false, true },
+                        { true , false, true, false },
+                        { false , true, false, true },
                 };
 
-        Assert.assertEquals(islandsCounter.countIsland(boolGrid), 8);
+        assertEquals( 8 , islandsCounter.countIslands(boolGrid));
+    }
+
+    @Test
+    public void countIslandInOneDimArr()
+    {
+        IslandsCounter islandsCounter = new IslandsCounter();
+
+        boolean[][] boolGrid = new boolean[][]
+                {
+                };
+
+        assertEquals( 0 , islandsCounter.countIslands(boolGrid));
     }
 }
