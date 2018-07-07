23b0af91ae7f59ddec36f0179c4953e4314d5319
Natalia Arciuchiewicz <naa316@ic.ac.uk>
2018-07-07 17:46:10 +0100

    Reformatted tests to make them easier to read

--- narciuch/assignment4/test/IslandFinderTest.java
+++ narciuch/assignment4/test/IslandFinderTest.java
@@ -7,14 +7,20 @@ public class IslandFinderTest {
 
   @Test
   public void findsMultipleIslands() {
-    boolean[][] land = {{false, true, false, true}, {true, true, false, false},
-      {false, false, true, false}, {false, false, true, false}};
+    boolean[][] land = {
+      {false, true, false, true},
+      {true, true, false, false},
+      {false, false, true, false},
+      {false, false, true, false}};
     assertEquals(islandFinder.getIslands(land), 3);
   }
 
   @Test
   public void maintainsOneIslandWhenAboveAndLeftCellArePartOfSameIsland() {
-    boolean[][] land = {{false, true, true, false}, {true, true, true, false}, {false, false, false, false}};
+    boolean[][] land = {
+      {false, true, true, false},
+      {true, true, true, false},
+      {false, false, false, false}};
     assertEquals(islandFinder.getIslands(land), 1);
   }
 
