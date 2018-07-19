42053e88ae9dfaaca5600c08765ea3a1602e6d0a
Olga Shiryaeva <algorithmhell@gmail.com>
2018-07-19 08:18:59 +0300

    Add files via upload
    Some comments on the code still need to be added

---
+++ group/assignment5/AlphabetDiscovererTest.java
@@ -0,0 +1,102 @@
+package assignment5;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+public class AlphabetDiscovererTest {
+    private AlphabetDiscoverer alphabetDiscoverer = new AlphabetDiscoverer();
+
+    @Test
+    public void testConsistentDictionary() {
+        List<String> dictionary = Arrays.asList(
+                "ART", "RAT", "CAT", "CAR"
+        );
+        List<List<Character>> possibleExpectedOutput = new ArrayList<>();
+        possibleExpectedOutput.add(Arrays.asList('A', 'T', 'R', 'C'));
+        possibleExpectedOutput.add(Arrays.asList('T', 'A', 'R', 'C'));
+
+        assertTrue(possibleExpectedOutput.contains(alphabetDiscoverer.getAlphabet(dictionary)));
+    }
+
+    @Test
+    public void testAllAlphabetsInConsistentDictionary() {
+        List<String> dictionary = Arrays.asList(
+                "ART", "RAT", "CAT", "CAR"
+        );
+        List<List<Character>> expectedResult = new ArrayList<>();
+        expectedResult.add(Arrays.asList('A', 'T', 'R', 'C'));
+        expectedResult.add(Arrays.asList('T', 'A', 'R', 'C'));
+        List<List<Character>> actualResult = alphabetDiscoverer.getAllPossibleAlphabets(dictionary);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInconsistentDictionary() {
+        List<String> dictionary = Arrays.asList(
+                "AB", "BC", "BA", "CC"
+        );
+        alphabetDiscoverer.getAlphabet(dictionary);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testEmptyDictionary() {
+        alphabetDiscoverer.getAlphabet(new ArrayList<>());
+    }
+
+    @Test()
+    public void testIllegalConstraints() {
+        List<String> dictionary = Arrays.asList(
+                "AB", "BC", "BA", "CC"
+        );
+        /*
+        A -> B -> C - |
+        <- - - - - - -
+         */
+        List<Constraint<Character>> expectedConstraints = Arrays.asList(
+                new Constraint<>('B', 'C'),
+                new Constraint<>('A', 'B'),
+                new Constraint<>('C', 'A')
+                );
+        Set<Constraint<Character>> actualConstraints = alphabetDiscoverer.getIllegalConstraints(dictionary);
+        assertTrue(actualConstraints.size() == 1);
+        assertTrue(expectedConstraints.contains(actualConstraints.iterator().next()));
+    }
+
+    @Test()
+    public void testIllegalConstraintsWithTwoCircuits() {
+        List<String> dictionary = Arrays.asList(
+                "AB", "BC", "BA", "CC", "D", "E", "FF", "FD"
+        );
+        /*
+        A -> B -> C -> | -> D -> E -> F - |
+        <- - - - - - -       < - - - - - -
+         */
+        List<List<Constraint<Character>>> expectedConstraintsGroups = Arrays.asList(
+                Arrays.asList(
+                new Constraint<>('B', 'C'),
+                new Constraint<>('A', 'B'),
+                new Constraint<>('C', 'A')
+                ),
+                Arrays.asList(
+                        new Constraint<>('D', 'E'),
+                        new Constraint<>('E', 'F'),
+                        new Constraint<>('F', 'D')
+                )
+        );
+        Set<Constraint<Character>> actualConstraints = alphabetDiscoverer.getIllegalConstraints(dictionary);
+        assertTrue(actualConstraints.size() == 2);
+        actualConstraints.forEach(e -> {
+            if(expectedConstraintsGroups.get(0).contains(e)) {
+                assertFalse(expectedConstraintsGroups.get(1).contains(e));
+            } else {
+                assertTrue(expectedConstraintsGroups.get(1).contains(e));
+            }
+        });
+    }
+}
