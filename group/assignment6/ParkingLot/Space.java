782e8ee466b154ed358e49c0fb057005afe2d611
Yu Jo <zoey5538@gmail.com>
2018-08-05 21:38:52 +0100

    Clean up more classes

--- group/assignment6/ParkingLot/Space.java
+++ group/assignment6/ParkingLot/Space.java
@@ -2,33 +2,39 @@ package assignment6.ParkingLot;
 
 import java.util.Objects;
 
+/**
+ * Represents a space with an ID.
+ */
 public class Space {
-    private final int id;
+  private final int id;
 
+  public Space(int id) {
+    this.id = id;
+  }
 
-    public Space(int id) {
-        this.id = id;
+  public int getId() {
+    return id;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
     }
-
-    public int getId() {
-        return id;
+    if (o == null || getClass() != o.getClass()) {
+      return false;
     }
+    Space space = (Space) o;
+    return id == space.id;
+  }
 
-    @Override
-    public String toString() {
-        return "Space " + id;
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Space space = (Space) o;
-        return id == space.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
+  @Override
+  public String toString() {
+    return "Space " + id;
+  }
 }
